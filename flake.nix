d7d6ec7ec78a7de2187bedc800859dd614b6690b
Peter Collingbourne <peter@pcc.me.uk>
2026-05-05 21:19:27 -0700

    switch to upstream

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.liminix = {
-    url = "github:pcc/liminix/flake-demo";
+    url = "github:telent/liminix";
     flake = false;
   };
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
